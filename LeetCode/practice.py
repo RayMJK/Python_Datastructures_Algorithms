8e789d043d935088133e897adc7055dbf90e949b
RayMJK <mjkraymond93@gmail.com>
2021-07-22 17:12:13 +0900

    7.22

--- LeetCode/practice.py
+++ LeetCode/practice.py
@@ -1,32 +1,18 @@
-# function that will print the map
-def mapping():
-    # taking no. of lines from the user
-    N = int(input("Enter the no. of lines you want to insert: "))
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    hash_map = {}
 
-    # list for storing the intial map
-    data = []
+    for i in range(len(nums)):
+        hash_map[nums[i]] = i
 
-    print("Enter the intial map")
-    for i in range(N):
-        data.append(input())
-        print(data)
-    print("\nFinal map")
-    # variable that will count no. of stars
-    k = 0
-    # iterating in the data
-    for i in data:
-        # if empty line encountered means new
-        # mapping now
-        if i == "":
-            print()
-            k = 0
-            continue
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        print(hash_map[complement])
+        if complement in hash_map and hash_map[complement] != i:
+            return [i, hash_map[complement]]
 
-        # counting and printing the results
-        c = i.count("*")
-        print("." * (len(i) - c - k), end="")
-        print("*" * c, end="")
-        print("." * k)
-        k += c
-
-mapping()
+print(twoSum([3,3],6))
